d7672b5f06515b6583fbb6f1a66a6639eb3c4998
Vika Shcherbak <vikashcherbaka@gmail.com>
2023-08-07 17:28:41 +0200

    add reset (eg.  reset colors)

--- minitalk.h
+++ minitalk.h
@@ -1,7 +1,6 @@
 #ifndef MINITALK_H_
 # define MINITALK_H_
 
-# define BLK "\e[0;30m"
 # define RED "\e[0;31m"
 # define GRN "\e[0;32m"
 # define YEL "\e[0;33m"
@@ -9,6 +8,7 @@
 # define MAG "\e[0;35m"
 # define CYN "\e[0;36m"
 # define WHT "\e[0;37m"
+# define RESET "\033[0m"
 
 #include <unistd.h>
 #include <stdlib.h>
